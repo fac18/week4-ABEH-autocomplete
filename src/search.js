c5e61ac4d8f7e53f18bd65bde5a82cf3e35cf4a0
Bethany Scott <bethanyioscott@hotmail.co.uk>
2019-11-20 16:57:33 +0000

    Write some tests and make them pass
    Co-authored-by: Gillian Yeomans <gillian@yeomans.org.uk>

--- src/search.js
+++ src/search.js
@@ -1,3 +1,14 @@
+const data = require("../data/data.js");
+let ballers = data.ballers;
+let birdList = data.birdList;
 
+
+function search(str) {
+  if (ballers.length < 6) {
+  return ballers;
+} else {
+  return ballers.slice(0, 5);
+}
+}
 
 module.exports = search;
