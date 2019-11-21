64d6b2338ebe1284ada42cd655d76357ad08477f
Maria <m.olanipekun@hotmail.com>
2019-11-21 11:46:52 +0000

    updating the handler/router files

--- src/handler.js
+++ src/handler.js
@@ -1,3 +1,45 @@
+const fs = require('fs');
+const path = require('path');
 
 
-module.exports = handler;
+const handleHome = (request, response) => {
+    const filePath = path.join(__dirname, '..', 'public', 'index.html');
+    fs.readFile(filePath, (error, file) => {
+        if (error) {
+            console.log(error);
+            response.writeHead(500, {'Content-type': 'text/html'});
+            response.end("<h1> Sorry we've had a problem");
+        } else {
+            response.writeHead(200, {'Content-type': 'text/html'});
+            response.end(file);
+        }
+    });
+};
+
+const handlePublic = (request,response,endpoint) => {
+    const extension = endpoint.split('.')[1]
+    const extensionType = {
+        html:'text/html',
+        css: 'text/css',
+        js: 'application/javascript',
+        json : 'application/json'
+    };
+    const filePath = path.join(__dirname, '..' , endpoint);
+    fs.readFile(filePath, (error , file)=> {
+       if(error){
+        console.log(error);
+        response.writeHead(404, {'Content-type': 'text/html'});
+        response.end("<h1> Sorry we've had a problem");  
+       } else{
+           response.writeHead(200,{'Content-type': extensionType[extension]});
+           response.end(file);
+       }  
+    });
+    console.log(endpoint);
+
+}
+
+
+  
+
+module.exports = {handlePublic,handleHome};
