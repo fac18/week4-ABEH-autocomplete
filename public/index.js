3ab5f0834c7d36e047699e815d7ef612760e834b
Gillian <gillian@yeomans.org.uk>
2019-11-22 12:27:39 +0000

    add up down and return select functionality

--- public/index.js
+++ public/index.js
@@ -2,17 +2,16 @@ const input = document.getElementById("userInput");
 const searchButton = document.getElementById("searchButton");
 
 let newArr = [];
-let navCounter = -1;
+let listPosition = -1;
 
 const changeValue = () => {
-  navCounter = -1;
+  listPosition = -1;
   let searchTerm = input.value;
   let xhr = new XMLHttpRequest();
   let searchUrl = `/search?q=${encodeURIComponent(searchTerm)}`;
   xhr.onreadystatechange = () => {
     if (xhr.readyState == 4 && xhr.status == 200) {
       newArr = JSON.parse(xhr.responseText);
-      console.log({ newArr });
       addSuggestions();
     }
   };
@@ -25,22 +24,11 @@ input.addEventListener("input", changeValue);
 //create li elements for each suggestion
 const addSuggestions = () => {
   const UlElement = document.querySelector(".dropdownList");
-  console.log({ UlElement });
   UlElement.textContent = "";
   newArr.forEach((suggestion, i) => {
     const liElement = document.createElement("li");
-    liElement.classList.add("suggestion-item");
-    const matchingText = document.createElement("span");
-    matchingText.classList.add("matching-text");
-    matchingText.textContent = suggestion.slice(0, input.value.length);
-    const remainingText = document.createElement("span");
-    remainingText.classList.add("remaining-text");
-    remainingText.textContent = suggestion.slice(input.value.length);
-    liElement.appendChild(matchingText);
-    liElement.appendChild(remainingText);
-    //adds highlight class to the element at the index decided by navCounter
-    //this index changes when arrow keys are used for navigating and the UL is rerendered each time
-    if (i === navCounter) {
+    liElement.textContent = suggestion;
+    if (i === listPosition) {
       liElement.classList.add("highlighted");
     }
     UlElement.appendChild(liElement);
@@ -56,15 +44,36 @@ const chooseSuggestion = event => {
   changeValue();
 };
 
-//40 is down
-
 searchButton.addEventListener("click", () => {
-  window.location.href = "https://en.wikipedia.org/wiki/" + input.value;
+  if (input.value) {
+    window.location.href = "https://en.wikipedia.org/wiki/" + input.value;
+  }
 });
 
+//code for keyboard navigation: return key to choose highlighted value from list, or search if already selected, up and down keys to navigate list
 input.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
     event.preventDefault();
-    searchButton.click();
+    if (document.querySelector(".highlighted")) {
+      input.value = document.querySelector(".highlighted").textContent;
+      input.focus();
+      changeValue();
+    } else {
+      searchButton.click();
+    }
+  } else if (event.keyCode === 40) {
+    if (listPosition < newArr.length - 1) {
+      listPosition++;
+      addSuggestions();
+    } else {
+      addSuggestions();
+    }
+  } else if (event.keyCode === 38) {
+    if (listPosition > 0) {
+      listPosition--;
+      addSuggestions();
+    } else {
+      addSuggestions();
+    }
   }
 });
